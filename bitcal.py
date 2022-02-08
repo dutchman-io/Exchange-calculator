a1ca67a39c96638f574891932f3a4a01cdbf4617
Mr-Dutchman <meelisfidelis@gmail.com>
2022-02-08 21:25:24 +0100

    ..

--- bitcal.py
+++ bitcal.py
@@ -1,36 +1,37 @@
 import urllib3
 import json
 http =urllib3.PoolManager()
+url = http.request('GET','https://bitcoinfees.earn.com/api/v1/fees/recommended')
+url_start = 'api.coincap.io/v2/assets/'
+
 
 def trans_cost():
-    url = http.request('GET','https://bitcoinfees.earn.com/api/v1/fees/recommended')
+    
     json_obj = json.loads(url.data.decode('utf-8'))
     return json_obj 
-json_obj1 = trans_cost() 
-#print(json_obj1)
+    json_obj1 = trans_cost()
+    print(json_obj1)
 
 
-def getPrice(coin_id):
+def getPrice():
     print("Input coin")
-    coin_id = input()
-    url_start = 'api.coincap.io/v2/assets/'
-    coin = coin_id
-    url = http.request('GET',str(url_start + coin))
+    coin = input()
+    url = http.request('GET', str(url_start + coin))
     json_obj = json.loads(url.data.decode('utf-8'))
     print(json_obj)
     
 def exchange_rate(currency, bitcoin):
     exchange_rate = currency/bitcoin
-#rate = exchange_rate()
-#print('Your bitcoin equals = ', rate)
+    rate = exchange_rate()
+    print('value equals = ', rate)
 
 
 def calc_sales_amount(bit_amount, trans_amount):
      amount = bit_amount + trans_amount
 
 
-#amount = calc_sales_amount()
-#print("You can sell at", amount, "without making a loss")
+amount = calc_sales_amount()
+print("You can sell at", amount, "without making a loss")
 
 """
 print('Amount in crypto choose  A and  Amount in  your currency choose B ')
